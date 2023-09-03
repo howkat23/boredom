6474e7b86edde0d7526af97a38cf3cdd450a340b
howkat23 <howardkaterina20@gmail.com>
2023-09-03 17:32:24 -0400

    your commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const [tips, setTips] = useState("");
+
+useEffect(() => {
+fetchTips();
+}, [])
+
+const fetchTips = async () => {
+const response = await fetch(`http://www.boredapi.com/api/activity/`);
+const data = await response.json();
+console.log(data.activity);
+setTips(data.activity)
+}
+
+
+return (
+
+<div className="App">
+<h1>How to Overcome Boredom</h1>
+    <div className="image">    
+        <img src="https://images.unsplash.com/photo-1562823319-5287af0d1acf?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDI2NnxGem8zenVPSE42d3x8ZW58MHx8fHx8&auto=format&fit=crop&w=500&q=60" alt='hobby'/>
+     </div>
+
+<p> {tips} </p>
+<button onClick={fetchTips}>New Tip</button>
+</div>
+
+
+);
 }
 
 export default App;
